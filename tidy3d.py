31ee5bf6219c6aa2dac582126f754223487ed44b
twhughes <tylerwhughes91@gmail.com>
2021-08-06 13:01:17 -0700

    adding tests

--- tidy3d.py
+++ tidy3d.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pydantic
-from typing import Tuple, Dict, List, Callable, Any
+from typing import Tuple, Dict, List, Callable, Any, Union
 
 import shutil
 
@@ -15,7 +15,6 @@ bound = Tuple[Tuple[float, float], Tuple[float, float], Tuple[float, float]]
 
 def validate_non_neg(field_name="size"):
     """makes sure all elements of field_name are non negative"""
-
     @pydantic.validator(field_name, allow_reuse=True, each_item=True)
     def is_non_neg(val):
         assert (
@@ -56,59 +55,39 @@ def check_bounds():
 
 class Geometry(pydantic.BaseModel):
     """defines where something exists in space"""
-    bounds: bound = None
 
     def __init__(self, **data: Any):
         """ checks the bounds after any Geometry instance is initialized """
         super().__init__(**data)
         _ = check_bounds()
 
-
-class Point(Geometry):
-    center: point
-    # bounds: bound = None
-
-    @pydantic.validator("bounds")
-    def set_bounds(cls, v, values):
-        print(f'setting bounds for {cls.__name__}')
-        center = values.get('center')
-        bounds = tuple((c, c) for c in center)
-        values['bounds'] = bounds
-        return bounds
-
 class Box(Geometry):
     size: size
     center: point
-    # bounds: bound = None
+    bounds: bound = None
 
     validate_non_neg("size")
 
     @pydantic.validator("bounds", always=True)
     def set_bounds(cls, v, values):
         print(f'setting bounds for {cls.__name__}')
-        print(values)
         size = values.get('size')
-        center = values.get('center')
+        center = values.get('center')     
         bounds = tuple((c - s/2.0, c + s/2.0) for (c, s) in zip(size, center))
         values['bounds'] = bounds
         return bounds
 
+class Point(Box):
+    def __init__(self, **data: Any):
+        data['size'] = (0., 0., 0.)
+        super().__init__(**data)
+
 class Plane(Box):
-    center: point
-    # bounds: bound = None
 
     @pydantic.validator("size")
     def check_size_has_one_zero(cls, v, values):       
-        assert v.count(0.0) == 1,  f"plane must have one element of size with value = 0.0, given {val}"
+        assert v.count(0.0) == 1,  f"plane must have one element of size with value = 0.0, given {v}"
         return v
-
-    @pydantic.validator("bounds", always=True)
-    def set_bounds(cls, v, values):
-        print(f'setting bounds for {cls.__name__}')
-        center = values.get('center')
-        bounds = tuple((c, c) for c in center)
-        values['bounds'] = bounds
-        return bounds
 
 """ ==== Structure ==== """
 
@@ -150,13 +129,28 @@ class Data(pydantic.BaseModel):
 
 
 class Mesh(pydantic.BaseModel):
-    size: size
-    dl: size
-    _ = validate_non_neg("size")
-    _ = validate_non_neg("dl")
+    resolution: size = None
+    step_size: size = None
 
+    @pydantic.root_validator
+    def set_mesh(cls, values):
+        resolution = values.get("resolution")
+        step_size = values.get("step_size")
+
+        assert (resolution is not None) or (step_size is not None), "either resolution or step size must be set"
+        assert (resolution is None) or (step_size is None), "resolution or step size can not both be set"
+        if resolution is None:
+            values["resolution"] = tuple(1 / dl for dl in step_size)
+        if step_size is None:
+            values["step_size"] = tuple(1 / res for res in resolution)
+        return values
+
+    _ = validate_non_neg("resolution")
+    _ = validate_non_neg("step_size")
 
 class Simulation(pydantic.BaseModel):
+    center: point = (0.0, 0.0, 0.0)
+    size: size
     mesh: Mesh
     structures: Dict[str, Structure]
     sources: Dict[str, Source]
@@ -165,9 +159,9 @@ class Simulation(pydantic.BaseModel):
 
 
 sim = Simulation(
+    size=(2.0, 2.0, 2.0),
     mesh=Mesh(
-        size=(1.0, 2.0, 1.0),
-        dl=(0.01, 0.01, 0.01),
+        step_size=(0.01, 0.01, 0.01),
     ),
     structures={
         "square": Structure(
@@ -235,3 +229,12 @@ if __name__ == "__main__":
     task_id = new_project(json)  # export to server
     load(sim, task_id)  # load data into sim.data containers
     # viz_data(sim, "plane")  # vizualize
+
+
+def test_geometry():
+    b = Box(size=(1, 1, 1), center=(0, 0, 0))
+    b = Box(size=(1, 1, 1), center=(0, 0, 0))
+    p = Plane(size=(1, 1, 1), center=(0, 0, 0))
+    p = Plane(size=(1, 1, 1), center=(0, 0, 0))
+    q = Point(size=(1, 1, 1), center=(0, 0, 0))
+    q = Point(size=(1, 1, 1), center=(0, 0, 0))
