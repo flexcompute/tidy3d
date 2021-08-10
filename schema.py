c8f6b5786ef8e9502652e31207d73945de10938d
twhughes <tylerwhughes91@gmail.com>
2021-08-10 09:15:22 -0700

    cuboid -> box, refactor geometry bounds, move time dependece out of mesh

--- schema.py
+++ schema.py
@@ -2,6 +2,7 @@ import json
 from jsonschema import Draft3Validator
 from jsonschema.validators import extend
 
+VALIDATOR = Draft3Validator
 
 def load_schema(fname_schema: str = "schema.json") -> dict:
     """loads schema from json file into dict"""
@@ -12,15 +13,15 @@ def load_schema(fname_schema: str = "schema.json") -> dict:
 
 def _accepts_tuple(checker, instance):
     """custom validator for array types, accept tuple as ok"""
-    is_array = Draft3Validator.TYPE_CHECKER.is_type(instance, "array")
+    is_array = VALIDATOR.TYPE_CHECKER.is_type(instance, "array")
     is_tuple = isinstance(instance, tuple)
     return is_array or is_tuple
 
 
-def create_validator(schema: dict) -> Draft3Validator:
+def _create_validator(schema: dict) -> Draft3Validator:
     """generates custom validator with array type accepting tuples"""
-    array_checker = Draft3Validator.TYPE_CHECKER.redefine("array", _accepts_tuple)
-    custom_validator = extend(Draft3Validator, type_checker=array_checker)
+    array_checker = VALIDATOR.TYPE_CHECKER.redefine("array", _accepts_tuple)
+    custom_validator = extend(VALIDATOR, type_checker=array_checker)
     validator = custom_validator(schema)
     return validator
 
@@ -28,5 +29,5 @@ def create_validator(schema: dict) -> Draft3Validator:
 def validate_schema(sim_dict: dict, fname_schema: str = "schema.json") -> None:
     """makes sure simulation dict is consistent with schema json"""
     schema_dict = load_schema(fname_schema)
-    validator = create_validator(schema_dict)
+    validator = _create_validator(schema_dict)
     validator.is_valid(sim_dict)
