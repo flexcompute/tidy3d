737d1192ba208a343668d30d772353ff2580937b
twhughes <tylerwhughes91@gmail.com>
2021-09-27 19:05:37 -0700

    added waveguide to tests

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -16,12 +16,16 @@ from tidy3d.plugins import ModeSolver
 
 
 def test_mode_solver():
+    waveguide = td.Structure(
+        geometry=td.Box(size=(td.inf, 0.5, 0.5)),
+        medium=td.Medium(permittivity=4.0))
     simulation = td.Simulation(
         size=(2, 2, 2),
         grid_size=0.1,
+        structures=[waveguide]
     )
-    plane = td.Box(center=(0, 0, 0), size=(1, 1, 0))
-    ms = ModeSolver(simulation=simulation, plane=plane, freq=1.0)
+    plane = td.Box(center=(0, 0, 0), size=(0, 2, 2))
+    ms = ModeSolver(simulation=simulation, plane=plane, freq=td.constants.C_0/1.5)
     # modes = ms.solve(mode=td.Mode(mode_index=1))
 
 
