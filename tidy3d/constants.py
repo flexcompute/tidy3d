3fef94eeade1a57165a9190f874dea17e00ca4f9
tylerflex <tyler@flexcompute.com>
2021-11-04 10:54:46 -0700

    added comments

--- tidy3d/constants.py
+++ tidy3d/constants.py
@@ -13,16 +13,20 @@ Attributes:
 
 import numpy as np
 
-EPSILON_0 = np.float32(8.85418782e-18)  # vacuum permittivity [F/um]
-MU_0 = np.float32(1.25663706e-12)  # vacuum permeability [H/um]
-C_0 = 1 / np.sqrt(EPSILON_0 * MU_0)  # speed of light in vacuum [um/s]
-ETA_0 = np.sqrt(MU_0 / EPSILON_0)  # vacuum impedance
-Q_e = 1.602176634e-19  # funamental charge
-HBAR = 6.582119569e-16  # reduced Planck constant [eV*s]
+# fundamental constants
+EPSILON_0 = np.float32(8.85418782e-18)
+MU_0 = np.float32(1.25663706e-12)
+C_0 = 1 / np.sqrt(EPSILON_0 * MU_0)
+ETA_0 = np.sqrt(MU_0 / EPSILON_0)
+Q_e = 1.602176634e-19
+HBAR = 6.582119569e-16
 
-inf = 1e16
+# infinity (very large)
+inf = 1e20
 
+# floating point precisions
 dp_eps = np.finfo(np.float64).eps
 fp_eps = np.finfo(np.float32).eps
 
+# values of PEC for mode solver
 pec_val = -1e8
