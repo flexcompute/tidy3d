e9aa4119a81573c1973e2e15b81ba2fe863a7b3c
tylerflex <tyler@flexcompute.com>
2022-01-24 12:35:32 -0800

    removed test_core

--- test_static.sh
+++ test_static.sh
@@ -6,7 +6,7 @@ pytest -rA tests/test_components.py
 pytest -rA tests/test_grid.py
 pytest -rA tests/test_IO.py
 pytest -rA tests/test_material_library.py
-pytest -rA tests/test_core.py
+# pytest -rA tests/test_core.py
 pytest -rA tests/test_plugins.py
 
 pytest --doctest-modules tidy3d \
