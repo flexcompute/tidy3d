48698e838fb118be67c171e8d0b1d91f80d30f3b
Tyler Hughes <92756888+tylerflex@users.noreply.github.com>
2022-01-27 16:51:57 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 # with open("tidy3d/requirements.txt") as f:
 #     required = f.read().splitlines()
 
-with open("docs/source/notebooks/requirements.txt") as f:
-    required = f.read().splitlines()
+# with open("docs/source/notebooks/requirements.txt") as f:
+#     required = f.read().splitlines()
     
 setup(
     packages=find_packages(),
