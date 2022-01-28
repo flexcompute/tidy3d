9c1864956caacaf69a3be5c1bb46b08c2b196cc3
Tyler Hughes <92756888+tylerflex@users.noreply.github.com>
2022-01-27 16:45:48 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+# with open("tidy3d/requirements.txt") as f:
+#     required = f.read().splitlines()
+
+with open("docs/source/notebooks/requirements.txt") as f:
+    required = f.read().splitlines()
+    
+setup(
+    packages=find_packages(),
+    install_requires=required,
+)
