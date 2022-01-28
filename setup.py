0febc506af7073b5bbae9a6c7bfccf53435c4c33
Tyler Hughes <92756888+tylerflex@users.noreply.github.com>
2022-01-27 16:56:09 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,10 @@ from setuptools import setup, find_packages
 # with open("tidy3d/requirements.txt") as f:
 #     required = f.read().splitlines()
 
-# with open("docs/source/notebooks/requirements.txt") as f:
-#     required = f.read().splitlines()
+with open("docs/source/notebooks/requirements.txt") as f:
+    required = f.read().splitlines()
     
 setup(
-    packages=find_packages(),
-    install_requires=[
-        numpy,
-        matplotlib,
-        tmm,
-        nlopt
-        tqdm,
-        gdspy,
-    ],
+    packages=find_packages(exclude=('tests',)),
+    install_requires = required
 )
