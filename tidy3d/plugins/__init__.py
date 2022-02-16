5bea0c61e728ced82f0b1eb1f52be073ee6582d2
tylerflex <tyler@flexcompute.com>
2022-02-16 15:20:42 -0800

    smatrix basics

--- tidy3d/plugins/__init__.py
+++ tidy3d/plugins/__init__.py
@@ -4,3 +4,5 @@ from .dispersion.fit import DispersionFitter
 from .dispersion.fit_web import StableDispersionFitter, AdvancedFitterParam
 from .mode.mode_solver import ModeSolver
 from .near2far.near2far import Near2Far, Near2FarSurface
+from .near2far.near2far import Near2Far
+from .smatrix.smatrix import ComponentModeler, Port
