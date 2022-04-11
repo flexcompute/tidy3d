27fb7e0be77ef9d1bc4278a2263cf4eb10928cfc
An Li <kidylee@gmail.com>
2022-04-11 17:17:29 +0800

    remove stateless_app.py

--- tidy3d/plugins/plotly/store.py
+++ tidy3d/plugins/plotly/store.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 
 from tidy3d import SimulationData
-from tidy3d.plugins.plotly import SimulationPlotly, SimulationDataApp
+from .simulation import SimulationPlotly
 from tidy3d.plugins.plotly.data import DataPlotly
 
 FrontEndStore = {}
@@ -9,12 +9,8 @@ FrontEndStore = {}
 
 class Store(ABC):
     @abstractmethod
-    def get_simulation_data(self, front_end_store: FrontEndStore):
+    def get_simulation_data(self, front_end_store: FrontEndStore) -> SimulationData:
         pass
-
-    def get_simulation_data(self, store: FrontEndStore) -> SimulationData:
-        data = self.get_simulation_data(store)
-        return SimulationPlotly(simulation=data.simulation)
 
     def get_simulation_plotly(self, store: []) -> SimulationPlotly:
         data = self.get_simulation_data(store)
@@ -32,7 +28,7 @@ class Store(ABC):
 class LocalStore(Store):
 
     def __init__(self, fname: str):
-        self.sim_data = SimulationDataApp.from_file(fname)
+        self.sim_data = SimulationData.from_file(fname)
 
     def get_simulation_data(self, front_end_store: FrontEndStore):
         return self.sim_data
@@ -40,18 +36,19 @@ class LocalStore(Store):
 
 class S3Store(Store):
 
-    def get_simulation_data(self, front_end_store: FrontEndStore):
+    def get_simulation_data(self, front_end_store: FrontEndStore) -> SimulationData:
         if front_end_store and front_end_store.get("task_id") == "1":
-            sim_data = SimulationDataApp.from_file("../../../data/monitor_data.hdf5")
+            sim_data = SimulationData.from_file("../../../data/monitor_data.hdf5")
         else:
-            sim_data = SimulationDataApp.from_file("../../../data/has_source.hdf5")
-        return sim_data.sim_data
+            sim_data = SimulationData.from_file("../../../data/has_source.hdf5")
+        return sim_data
 
 
 _DEFAULT_STORE = S3Store()
 
 
 def set_store(store: Store):
+    global _DEFAULT_STORE
     _DEFAULT_STORE = store
 
 
