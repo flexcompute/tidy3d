231c9377ebb8b6c3f323d8a3199a09b9db1a7e94
tylerflex <tyler@flexcompute.com>
2022-05-19 12:02:15 -0700

    fixes pylint bug with subscriptable None value

--- tidy3d/web/s3utils.py
+++ tidy3d/web/s3utils.py
@@ -2,15 +2,19 @@
 import boto3
 
 from .config import DEFAULT_CONFIG as Config
+from ..log import WebError
 
 
 def get_s3_client():
     """returns the client based on Config"""
-    keys = Config.user
+    user = Config.user
+    if user is None:
+        raise WebError("Could not get s3 client, user not defined.")
+
     return boto3.client(
         "s3",
-        aws_access_key_id=keys["userAccessKey"],
-        aws_secret_access_key=keys["userSecretAccessKey"],
+        aws_access_key_id=user.get("userAccessKey"),
+        aws_secret_access_key=user.get("userSecretAccessKey"),
         region_name=Config.s3_region,
     )
 
@@ -19,7 +23,10 @@ def get_s3_user():
     """gets all information relevant to user's Config for s3"""
     client = get_s3_client()
     bucket = Config.studio_bucket
-    user_id = Config.user["UserId"]
+    user = Config.user
+    if user is None:
+        raise WebError("Could not get s3 client, user not defined.")
+    user_id = user.get("UserId")
     return client, bucket, user_id
 
 
