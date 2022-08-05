a1dde36bdf608c6defbd831bb1358e8faa0d925d
Daniil <daniil@flexcompute.com>
2022-08-04 20:29:04 -0500

    moving precision switching back to backend

--- tidy3d/constants.py
+++ tidy3d/constants.py
@@ -13,21 +13,19 @@ Attributes:
 
 import numpy as np
 
-# floating point types and precisions
-TIDY3D_DOUBLE_PRECISION = False
-fp_type = np.float64 if TIDY3D_DOUBLE_PRECISION else np.float32
-cfp_type = np.complex128 if TIDY3D_DOUBLE_PRECISION else np.complex64
-fp_eps = np.finfo(fp_type).eps
-
 # fundamental constants
-EPSILON_0 = fp_type(8.85418782e-18)
-MU_0 = fp_type(1.25663706e-12)
+EPSILON_0 = 8.85418782e-18
+MU_0 = 1.25663706e-12
 C_0 = 1 / np.sqrt(EPSILON_0 * MU_0)
 
 #: Free space impedance
 ETA_0 = np.sqrt(MU_0 / EPSILON_0)
 Q_e = 1.602176634e-19
 HBAR = 6.582119569e-16
+
+# floating point precisions
+dp_eps = np.finfo(np.float64).eps
+fp_eps = np.finfo(np.float32).eps
 
 # values of PEC for mode solver
 pec_val = -1e8
