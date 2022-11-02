857cb088ba3c3ddd57e8f0b5ae289200158e7e64
momchil <momchil@flexcompute.com>
2022-11-01 17:13:33 -0700

    Updating CustomFieldSource and tests; adding FieldData.to_source

--- tidy3d/components/data/monitor_data.py
+++ tidy3d/components/data/monitor_data.py
@@ -16,13 +16,14 @@ from .data_array import ScalarFieldDataArray, ScalarFieldTimeDataArray
 from .dataset import Dataset, AbstractFieldDataset, ElectromagneticFieldDataset
 from .dataset import FieldDataset, FieldTimeDataset, ModeSolverDataset, PermittivityDataset
 from ..base import TYPE_TAG_STR
-from ..types import Coordinate, Symmetry, ArrayLike
+from ..types import Coordinate, Symmetry, ArrayLike, Size
 from ..grid.grid import Grid
 from ..validators import enforce_monitor_fields_present, required_if_symmetry_present
 from ..monitor import MonitorType, FieldMonitor, FieldTimeMonitor, ModeSolverMonitor
 from ..monitor import ModeMonitor, FluxMonitor, FluxTimeMonitor, PermittivityMonitor
 from ..monitor import Near2FarAngleMonitor, Near2FarCartesianMonitor, Near2FarKSpaceMonitor
 from ..monitor import DiffractionMonitor
+from ..source import SourceTimeType, CustomFieldSource
 from ..medium import Medium, MediumType
 from ...log import SetupError, DataError
 from ...constants import ETA_0, C_0, MICROMETER
@@ -331,6 +332,43 @@ class FieldData(FieldDataset, ElectromagneticFieldData):
             fields_norm[field_name] = (field_data / src_amps).astype(field_data.dtype)
 
         return self.copy(update=fields_norm)
+
+    def to_source(
+        self, source_time: SourceTimeType, center: Coordinate, size: Size = None
+    ) -> CustomFieldSource:
+        """Create a :class:`.CustomFieldSource` from the fields stored in the :class:`.FieldData`.
+
+        Parameters
+        ----------
+        source_time: :class:`.SourceTime`
+            Specification of the source time-dependence.
+        center: Tuple[float, float, float]
+            Source center in x, y and z.
+        size: Tuple[float, float, float]
+            Source size in x, y, and z. If not provided, the size of the monitor associated to the
+            data is used.
+
+        Returns
+        -------
+        :class:`.CustomFieldSource`
+            Source injecting the fields stored in the :class:`.FieldData`, with other settings as
+            provided in the input arguments.
+        """
+
+        if not size:
+            size = self.monitor.size
+
+        fields = {}
+        for name, field in self.symmetry_expanded_copy.field_components.items():
+            fields[name] = field.copy()
+            for dim, dim_name in enumerate("xyz"):
+                coords_shift = field.coords[dim_name] - self.monitor.center[dim]
+                fields[name].coords[dim_name] = coords_shift
+
+        dataset = FieldDataset(**fields)
+        return CustomFieldSource(
+            field_dataset=dataset, source_time=source_time, center=center, size=size
+        )
 
 
 class FieldTimeData(FieldTimeDataset, ElectromagneticFieldData):
