2c97db8fc9f0698377fa097a5821ff13e9febaed
Tyler Hughes <tyler@flexcompute.com>
2023-01-10 14:12:53 -0600

    renamed logger to avoid conflicts, added test

--- tests/test_package/test_log.py
+++ tests/test_package/test_log.py
@@ -4,6 +4,7 @@ import pytest
 import pydantic as pd
 import tidy3d as td
 from tidy3d.log import Tidy3dError, ConfigError, set_logging_level
+from tidy3d.log import DEFAULT_LEVEL, _get_level_int
 
 log = td.log
 
@@ -34,3 +35,27 @@ def test_exception_message():
     MESSAGE = "message"
     e = Tidy3dError(MESSAGE)
     assert str(e) == MESSAGE
+
+
+def test_reset_logging_level():
+
+    import logging
+    from rich.logging import RichHandler
+
+    td.config.logging_level = DEFAULT_LEVEL.lower()
+    LEVEL = logging.ERROR
+    assert DEFAULT_LEVEL != LEVEL, "set LEVEL something different from the DEFAULT_LEVEL for tidy3d"
+
+    logging.basicConfig(
+        level=LEVEL,
+        format="%(message)s",
+        datefmt="[%X]",
+        handlers=[RichHandler(rich_tracebacks=True)],
+    )
+
+    log_test = logging.getLogger("rich")
+    log_test.setLevel(LEVEL)
+    assert log_test.level == LEVEL, "test logger level wasnt set correctly."
+    assert log.level == _get_level_int(
+        DEFAULT_LEVEL.lower()
+    ), "tidy3d log level was reset by making a new logger"
