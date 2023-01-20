045ceea36f7130625cf0349af3c4a797828c8416
Tyler Hughes <tyler@flexcompute.com>
2023-01-20 12:58:28 -0600

    add missing requirement and fix test

--- tests/test_web/test_webapi.py
+++ tests/test_web/test_webapi.py
@@ -23,6 +23,7 @@ from tidy3d.web.webapi import (
     upload,
 )
 
+
 def make_sim():
     """Makes a simulation."""
     return td.Simulation(size=(1, 1, 1), grid_spec=td.GridSpec.auto(wavelength=1.0), run_time=1e-12)
