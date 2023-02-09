984a0347d6f2e1e4a8661e919b7a67225b09a82d
Tyler Hughes <tyler@flexcompute.com>
2023-02-09 12:53:46 -0600

    compatible jax 0.3 or 0.4, add performance scaling

--- tests/_test_local/_test_adjoint_performance.py
+++ tests/_test_local/_test_adjoint_performance.py
@@ -3,6 +3,8 @@ import numpy as np
 import os
 import sys
 from memory_profiler import profile
+import matplotlib.pylab as plt
+import time
 
 from tidy3d.components.data.sim_data import SimulationData
 from tidy3d.components.data.monitor_data import FieldData
@@ -62,12 +64,15 @@ BASE_EPS_VAL = 2.0
 # name of the output monitor used in tests
 MNT_NAME = "mode"
 
-Nx, Ny, Nz = 300, 300, 1
-EPS_VALUES = 1 + np.random.random((Nx, Ny, Nz, 1))
+
+# 300 : 7448/7379
+# 300 : 7448/7379
 
 
 def make_sim(eps_values: np.ndarray) -> JaxSimulation:
     """Construt a simulation out of some input parameters."""
+
+    Nx, Ny, Nz, Nf = eps_values.shape
 
     box = td.Box(size=(1, 1, 1), center=(1, 2, 2))
     med = td.Medium(permittivity=2.0)
@@ -181,6 +186,109 @@ def test_large_custom_medium(use_emulated_run):
         amp = extract_amp(sim_data)
         return objective(amp)
 
+    Nx, Ny, Nz = 100, 100, 1
+    EPS_VALUES = 1 + np.random.random((Nx, Ny, Nz, 1))
+
     with Profile():
         grad_f = grad(f)
         df_eps_values = grad_f(EPS_VALUES)
+
+
+def test_time_custom_medium(use_emulated_run):
+
+    num_tests = 50
+    nxs = np.logspace(0, 2.0, num_tests)
+    times_sec = np.zeros(num_tests)
+    num_pixels = np.zeros(num_tests)
+
+    def f(eps_values):
+        sim = make_sim(eps_values=eps_values)
+        sim_data = run(sim, task_name="test")
+        amp = extract_amp(sim_data)
+        return objective(amp)
+
+    g = grad(f)
+
+    # run it once to compile for accurate comparison
+    g(np.ones((1, 1, 1, 1)))
+
+    for i, nx in enumerate(nxs):
+        nx = int(nx)
+        ny = nx
+        eps_array = 1 + np.random.random((nx, ny, 1, 1))
+
+        tstart = time.time()
+        deps = g(eps_array)
+        tend = time.time()
+        times_sec[i] = tend - tstart
+        num_pixels[i] = eps_array.size
+
+    plt.scatter(num_pixels, times_sec)
+    plt.plot(num_pixels, times_sec)
+    plt.xlabel("number of pixels")
+    plt.ylabel("grad(eps) time (sec)")
+    plt.yscale("log")
+    plt.xscale("log")
+    plt.show()
+
+
+def test_simple_jax_data_array(use_emulated_run):
+    def make_data_array(values):
+        coords = {}
+        for dim, n in zip("xyzf", values.shape):
+            coords[dim] = np.linspace(0, 1, n).tolist()
+        return JaxDataArray(values=values, coords=coords)
+
+    def make_custom_medium(values):
+        components = {f"eps_{dim}{dim}": make_data_array(values) for dim in "xyz"}
+        eps_dataset = JaxPermittivityDataset(**components)
+        return JaxCustomMedium(eps_dataset=eps_dataset)
+
+    def make_custom_structure(values):
+        medium = make_custom_medium(values)
+        geometry = JaxBox(size=(1, 1, 1), center=(0.5, 0.5, 0.5))
+        return JaxStructure(geometry=geometry, medium=medium)
+
+    def make_sim(values):
+        structure = make_custom_structure(values)
+        output_mnt = td.ModeMonitor(
+            size=(10, 10, 0),
+            mode_spec=td.ModeSpec(num_modes=3),
+            freqs=[FREQ0],
+            name="mode",
+        )
+        return JaxSimulation(
+            size=(1, 1, 1),
+            center=(0.5, 0.5, 0.5),
+            grid_spec=td.GridSpec.auto(wavelength=0.1),
+            input_structures=[structure],
+            output_monitors=[output_mnt],
+            run_time=1e-12,
+        )
+
+    Nx, Ny, Nz, Nf = 300, 300, 1, 1
+    VALUES = 2 + np.random.random((Nx, Ny, Nz, Nf))
+    data_array = make_data_array(VALUES)
+
+    # the first 3 are fast, last one is slow
+
+    def f(values):
+        data_array = make_data_array(values)
+        return jnp.sum(data_array.values)
+
+    def f(values):
+        custom_medium = make_custom_medium(values)
+        return jnp.sum(custom_medium.eps_dataset.eps_xx.values)
+
+    def f(values):
+        custom_structure = make_custom_structure(values)
+        return jnp.sum(custom_structure.medium.eps_dataset.eps_xx.values)
+
+    def f(values):
+        sim = make_sim(values)
+        sim_data = run(sim, task_name="test")
+        return abs(sim_data["mode"].amps.sel(f=FREQ0, mode_index=0, direction="+"))
+
+    with Profile():
+        g = grad(f)
+        g(VALUES)
