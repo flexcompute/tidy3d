bacb6ceab6b65bf75683abfbbf51d405795f62e8
Tyler Hughes <tyler@flexcompute.com>
2023-02-28 12:18:10 -0600

    variables for mock functions

--- tidy3d/web/http_management.py
+++ tidy3d/web/http_management.py
@@ -1,6 +1,5 @@
-"""
-Http connection pool and authentication management
-"""
+"""Http connection pool and authentication management."""
+
 import os
 from functools import wraps
 from os.path import expanduser
@@ -23,11 +22,9 @@ class ResponseCodes(Enum):
     NOT_FOUND = 404
 
 
-def api_key():
-    """
-    Get the api key for the current environment.
-    :return:
-    """
+def api_key() -> None:
+    """Get the api key for the current environment."""
+
     if os.environ.get(SIMCLOUD_APIKEY):
         return os.environ.get(SIMCLOUD_APIKEY)
     if os.path.exists(f"{expanduser('~')}/.tidy3d/config"):
@@ -38,11 +35,18 @@ def api_key():
     return None
 
 
-def api_key_auth(request):
-    """
-    Set the authentication.
-    :param request:
-    :return:
+def api_key_auth(request: requests.request) -> requests.request:
+    """Save the authentication info in a request.
+
+    Parameters
+    ----------
+    request : requests.request
+        The original request to set authentication for.
+
+    Returns
+    -------
+    requests.request
+        The request with authentication set.
     """
     key = api_key()
     if not key:
@@ -57,15 +61,11 @@ def api_key_auth(request):
 
 
 def http_interceptor(func):
-    """
-    Intercept the response and raise an exception if the status code is not 200.
-    :param func:
-    :return:
-    """
+    """Intercept the response and raise an exception if the status code is not 200."""
 
     @wraps(func)
     def wrapper(*args, **kwargs):
-        """A wrapper function"""
+        """The wrapper function."""
 
         # Extend some capabilities of func
         resp = func(*args, **kwargs)
@@ -84,53 +84,32 @@ def http_interceptor(func):
 
 
 class HttpSessionManager:
-    """
-    Http util class.
-    """
+    """Http util class."""
 
     def __init__(self, session: requests.Session):
+        """Initialize the session."""
         self.session = session
 
     @http_interceptor
     def get(self, path: str, json=None):
-        """
-        Get the resource.
-        :param path:
-        :param json:
-        :return:
-        """
+        """Get the resource."""
         return self.session.get(url=Env.current.get_real_url(path), auth=api_key_auth, json=json)
 
     @http_interceptor
     def post(self, path: str, json=None):
-        """
-        Create the resource.
-        :param path:
-        :param json:
-        :return:
-        """
+        """Create the resource."""
         return self.session.post(Env.current.get_real_url(path), json=json, auth=api_key_auth)
 
     @http_interceptor
     def put(self, path: str, json=None, files=None):
-        """
-        Update the resource.
-        :param files:
-        :param path:
-        :param json:
-        :return:
-        """
+        """Update the resource."""
         return self.session.put(
             Env.current.get_real_url(path), data=json, auth=api_key_auth, files=files
         )
 
     @http_interceptor
     def delete(self, path: str):
-        """
-        Delete the resource.
-        :param path:
-        :return:
-        """
+        """Delete the resource."""
         return self.session.delete(Env.current.get_real_url(path), auth=api_key_auth)
 
 
