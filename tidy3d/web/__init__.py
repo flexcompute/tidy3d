6a25bc16aadfb2e6c6073fae39e32b00cc658c25
momchil <momchil@flexcompute.com>
2023-02-28 17:03:52 -0800

    Removing billed cost display; adding real_cost function for user

--- tidy3d/web/__init__.py
+++ tidy3d/web/__init__.py
@@ -2,7 +2,7 @@
 import sys
 
 from .webapi import run, upload, get_info, start, monitor, delete, download, load, estimate_cost
-from .webapi import get_tasks, delete_old, download_json, download_log, load_simulation
+from .webapi import get_tasks, delete_old, download_json, download_log, load_simulation, real_cost
 from .container import Job, Batch, BatchData
 from .auth import get_credentials
 from .cli import tidy3d_cli
