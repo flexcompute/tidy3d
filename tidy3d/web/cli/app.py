30c23e005f577b017869e24c0d68736efdc4d337
Kidy Lee <lian@magicloud.io>
2023-03-22 13:51:07 +0800

    add auth.json migration

--- tidy3d/web/cli/app.py
+++ tidy3d/web/cli/app.py
@@ -1,23 +1,35 @@
 """
 Commandline interface for tidy3d.
 """
+import json
 import os.path
-from os.path import expanduser
 
 import click
 import requests
 import toml
 
+from tidy3d.web.cli.constants import TIDY3D_DIR, CONFIG_FILE, CREDENTIAL_FILE
+from tidy3d.web.cli.migrate import migrate
 from tidy3d.web.config import DEFAULT_CONFIG
 
-TIDY3D_DIR = f"{expanduser('~')}/.tidy3d"
-CONFIG_FILE = TIDY3D_DIR + "/config"
+if not os.path.exists(TIDY3D_DIR):
+    os.mkdir(TIDY3D_DIR)
 
-if os.path.exists(CONFIG_FILE):
-    with open(CONFIG_FILE, "r", encoding="utf-8") as f:
-        content = f.read()
-        config = toml.loads(content)
-        config_description = f"API Key:\nCurrent:[{config.get('apikey', '')}]\nNew"
+
+def get_description():
+    """Get the description for the config command.
+    Returns
+    -------
+    str
+        The description for the config command.
+    """
+
+    if os.path.exists(CONFIG_FILE):
+        with open(CONFIG_FILE, "r", encoding="utf-8") as f:
+            content = f.read()
+            config = toml.loads(content)
+            return config.get("apikey", "")
+    return ""
 
 
 @click.group()
@@ -28,9 +40,7 @@ def tidy3d_cli():
 
 
 @click.command()
-@click.option(
-    "--apikey", prompt=config_description if "config_description" in globals() else "API Key"
-)
+@click.option("--apikey", prompt=False)
 def configure(apikey):
     """Click command to configure the api key.
     Parameters
@@ -53,11 +63,24 @@ def configure(apikey):
         req.headers["simcloud-api-key"] = apikey
         return req
 
-    resp = requests.get(f"{DEFAULT_CONFIG.web_api_endpoint}/apikey", auth=auth)
+    if os.path.exists(CREDENTIAL_FILE):
+        with open(CREDENTIAL_FILE, "r", encoding="utf-8") as fp:
+            auth_json = json.load(fp)
+        email = auth_json["email"]
+        password = auth_json["password"]
+        if email and password:
+            if migrate():
+                click.echo("Migrate successfully. auth.json is renamed to auth.json.bak.")
+                return
+
+    if not apikey:
+        current_apikey = get_description()
+        message = f"Current API key: [{current_apikey}]\n" if current_apikey else ""
+        apikey = click.prompt(f"{message}Please enter your api key", type=str)
+
+    resp = requests.get(f"{DEFAULT_CONFIG.web_api_endpoint}/apikey", auth=auth, verify=False)
     if resp.status_code == 200:
         click.echo("Configured successfully.")
-        if not os.path.exists(TIDY3D_DIR):
-            os.mkdir(TIDY3D_DIR)
         with open(CONFIG_FILE, "w+", encoding="utf-8") as config_file:
             toml_config = toml.loads(config_file.read())
             toml_config.update({"apikey": apikey})
@@ -66,4 +89,11 @@ def configure(apikey):
         click.echo("API key is invalid.")
 
 
+@click.command()
+def migration():
+    """Click command to migrate the credential to api key."""
+    migrate()
+
+
 tidy3d_cli.add_command(configure)
+tidy3d_cli.add_command(migration)
