067b47f551c71fdc8cebfbea6cc1676d9bcb7232
Lius-fe <Lius-fe>
2023-06-25 18:22:26 +0800

    feat: exclude examples page

--- docs/source/_ext/custom-meta.py
+++ docs/source/_ext/custom-meta.py
@@ -2,7 +2,7 @@ import os
 
 def html_page_context(app, pagename, templatename, context, doctree):
     notebook_path = app.env.doc2path(os.path.abspath("" +pagename), base=None)
-    if notebook_path.endswith('.ipynb') or notebook_path.endswith('.ipynb.txt'):
+    if "notebook" in notebook_path or notebook_path.endswith('examples.rst'):
         context['metatags'] += "".join(['\n\t<meta content="noindex" name="robots" />'])
 
 def setup(app):
