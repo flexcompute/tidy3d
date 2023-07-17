798bddd79ac3999f4f42d30bc8a7df5fbd0396f8
flex-Alec <139144665+alec-flexcompute@users.noreply.github.com>
2023-07-17 12:01:04 -0700

    []

--- tidy3d/plugins/mode/mode_solver.py
+++ tidy3d/plugins/mode/mode_solver.py
@@ -673,7 +673,7 @@ class ModeSolver(Tidy3dBaseModel):
         else:
             new_monitors = new_monitors + tuple(mode_solver_monitor)
         print(new_monitors)
-        newSim = self.simulation.copy(update=dict(monitors=new_monitors))
+        newSim = self.simulation.copy(update=dict(monitors=[new_monitors]))
 
         return newSim
 
