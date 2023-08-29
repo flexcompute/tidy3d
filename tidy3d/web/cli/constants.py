9f2cce63e66c989fcaaf11aa1932cf497db73532
Yannick Augenstein <1576526+yaugenst@users.noreply.github.com>
2023-08-29 16:06:34 -0700

    Make TIDY3D_DIR configurable via environment variable

--- tidy3d/web/cli/constants.py
+++ tidy3d/web/cli/constants.py
@@ -1,6 +1,7 @@
 """Constants for the CLI."""
+from os import getenv
 from os.path import expanduser
 
-TIDY3D_DIR = f"{expanduser('~')}/.tidy3d"
+TIDY3D_DIR = getenv("TIDY3D_DIR", f"{expanduser('~')}/.tidy3d")
 CONFIG_FILE = TIDY3D_DIR + "/config"
 CREDENTIAL_FILE = TIDY3D_DIR + "/auth.json"
