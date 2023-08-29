677ef55ca21b6675f80c7404ea3b6ca9c33ee791
Yannick Augenstein <1576526+yaugenst@users.noreply.github.com>
2023-08-29 16:52:50 -0700

    Add TIDY3D_BASE_DIR from environment and autoselect tmp directory if default is not writable

--- tidy3d/web/cli/constants.py
+++ tidy3d/web/cli/constants.py
@@ -1,7 +1,14 @@
 """Constants for the CLI."""
-from os import getenv
+
+import os
 from os.path import expanduser
 
-TIDY3D_DIR = getenv("TIDY3D_DIR", f"{expanduser('~')}/.tidy3d")
+TIDY3D_BASE_DIR = os.getenv("TIDY3D_BASE_DIR", f"{expanduser('~')}")
+
+if os.access(TIDY3D_BASE_DIR, os.W_OK):
+    TIDY3D_DIR = f"{TIDY3D_BASE_DIR}/.tidy3d"
+else:
+    TIDY3D_DIR = "/tmp/.tidy3d"
+
 CONFIG_FILE = TIDY3D_DIR + "/config"
 CREDENTIAL_FILE = TIDY3D_DIR + "/auth.json"
