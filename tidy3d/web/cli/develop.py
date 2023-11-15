68a276a6d74582fea149e68fb0da2f68b823468b
Dario Quintero <dario@flexcompute.com>
2023-11-15 16:38:07 +0100

    FEAT: Environment verificaiton and configuration scripts alright

--- tidy3d/web/cli/develop.py
+++ tidy3d/web/cli/develop.py
@@ -1,6 +1,34 @@
 """Console script for tidy3d."""
 import click
 import subprocess
+import re
+
+
+def verify_pandoc_is_installed_and_version_less_than_3():
+    try:
+        # Running 'pandoc --version' command
+        result = subprocess.run(["pandoc", "--version"], capture_output=True, text=True, check=True)
+
+        # Extracting the version number from the output
+        version_match = re.search(r"pandoc\s+(\d+\.\d+.\d+)", result.stdout)
+        if version_match:
+            version = version_match.group(1)
+            major_version = int(version.split(".")[0])
+
+            if major_version < 3:
+                print(f"Pandoc is installed with version {version}, which is less than 3.")
+                return True
+            else:
+                print(f"Pandoc version {version} is installed, but it is not less than 3.")
+                return False
+        else:
+            print("Pandoc version number could not be determined.")
+            return False
+
+    except subprocess.CalledProcessError:
+        # This exception is raised if the command returned a non-zero exit status
+        print("Pandoc is not installed or not found in the system PATH.")
+        return False
 
 
 def verify_pipx_is_installed():
@@ -12,7 +40,7 @@ def verify_pipx_is_installed():
         if result.returncode == 0:
             print("pipx is installed: " + result.stdout)
             return True
-    except:
+    except subprocess.CalledProcessError:
         # This exception is raised if the command returned a non-zero exit status
         print("pipx is not installed or not found in the system PATH.")
         return False
@@ -26,9 +54,29 @@ def verify_poetry_is_installed():
         if result.returncode == 0:
             print("Poetry is installed: " + result.stdout)
             return True
-    except:
+    except subprocess.CalledProcessError:
         # This exception is raised if the command returned a non-zero exit status
         raise OSError("Poetry is not installed or not found in the system PATH.")
+
+
+def verify_sphinx_is_installed():
+    # TODO: Not working don't know why?
+    try:
+        # Running 'poetry --version' command
+        subprocess.run(["poetry", "env", "use", "python"])
+        result = subprocess.run(
+            ["poetry", "run", "python -m", "sphinx --version"],
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+        # If the command was successful, we'll get the version info
+        if result.returncode == 0:
+            print("sphinx is installed: " + result.stdout)
+            return True
+    except subprocess.CalledProcessError:
+        # This exception is raised if the command returned a non-zero exit status
+        raise OSError("sphinx is not installed or not found in the poetry environment.")
 
 
 @click.group(name="develop")
@@ -46,7 +94,10 @@ def verify_development_environment(args=None):
     verify_pipx_is_installed()
     # Verify poetry is installed
     verify_poetry_is_installed()
+    # Verify pandoc is installed
+    verify_pandoc_is_installed_and_version_less_than_3()
     # Dry run the poetry install to understand the configuration
+    subprocess.run(["poetry", "env", "use", "python"])
     subprocess.run(["poetry", "install", "-E dev", "--dry-run"])
     print(
         "`poetry install -E dev` dry run on the `poetry.lock` complete.\nManually verify packages are properly installed."
@@ -61,17 +112,33 @@ def configure_development_environment(args=None):
     try:
         verify_pipx_is_installed()
     except:
-        subprocess.run(["python -m pip install --user pipx"])
-        subprocess.run(["python -m -m pipx ensurepath"])
+        subprocess.run(["python -m", "pip", "install", "--user pipx"])
+        subprocess.run(["python -m", "pipx", "ensurepath"])
 
     # Verify and install poetry if required
     try:
         verify_poetry_is_installed()
     except:
-        subprocess.run(["pipx install poetry"])
+        subprocess.run(["pipx", "install", "poetry"])
+
+    # Verify pandoc is installed
+    try:
+        verify_pandoc_is_installed_and_version_less_than_3()
+    except:
+
+        raise OSError(
+            "Please install pandoc < 3 depending on your platform: https://pandoc.org/installing.html . Then run this command again."
+        )
 
     # Makes sure the package has installed all the development dependencies.
     subprocess.run(["poetry", "install", "-E dev"])
+
+    # # Verify the sphinx installation.
+    # try:
+    #     verify_sphinx_is_installed()
+    # except:
+    #     print("There has been an error on your poetry sphinx installation.\nManually run `poetry run python -m sphinx --version` to verify.")
+
     return 0
 
 
