6a05c9e83ebc74e446ffc46c541636add81ae61a
Dario Quintero <dario@flexcompute.com>
2023-11-15 13:47:38 +0100

    FEAT: Starting off making develop helper commands

--- tidy3d/web/cli/develop.py
+++ tidy3d/web/cli/develop.py
@@ -5,20 +5,47 @@ import click
 
 @click.group(name="develop")
 def develop():
-    """Development related tasks."""
+    """Development related commands."""
     pass
+
+
+@develop.command(name="configure-dev-environment", help="Verifies the development environment.")
+def configure_development_environment(args=None):
+    """Configure development environment."""
+    # Makes sure the package has installed all the development dependencies.
+    click.echo("Replace this message by putting your code into " "tidy3d.cli.main")
+    click.echo("See click documentation at https://click.palletsprojects.com/")
+    return 0
 
 
 @develop.command(name="verify-dev-environment", help="Verifies the development environment.")
 def verify_development_environment(args=None):
+    # Does all the docs verifications
+    # Checks all the other development dependencies are properly installed
     """Verifies that the environment in which this is run conforms to the provided poetry.lock with all the development options included."""
     click.echo("Replace this message by putting your code into " "tidy3d.cli.main")
     click.echo("See click documentation at https://click.palletsprojects.com/")
     return 0
 
 
-@develop.command(name="verify-docs-configuration", help="Verifies the documentation configuration.")
+@develop.command(
+    name="configure-docs-environment", help="Verifies the documentation configuration."
+)
+def configure_documentation_environment(args=None):
+    # Sets up the notebook submodule and updates it.
+    # Installs pandoc if required.
+    # Runs the documentation verificaiton script
+    """Verifies that the environment in which this is run conforms to the provided poetry.lock with all the development options included."""
+    click.echo("Replace this message by putting your code into " "tidy3d.cli.main")
+    click.echo("See click documentation at https://click.palletsprojects.com/")
+    return 0
+
+
+@develop.command(name="verify-docs-environment", help="Verifies the documentation configuration.")
 def verify_documentation_configuration(args=None):
+    # Checks pandoc version
+    # Checks documentation depednencies match
+    # Checks submodule is updated
     """Verifies that the environment in which this is run conforms to the provided poetry.lock with all the development options included."""
     click.echo("Replace this message by putting your code into " "tidy3d.cli.main")
     click.echo("See click documentation at https://click.palletsprojects.com/")
