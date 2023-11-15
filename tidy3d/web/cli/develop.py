e84e9b6d4f1bf55f6455d66ae21924d3b69ef263
Dario Quintero <dario@flexcompute.com>
2023-11-15 16:14:12 +0100

    FEAT: Environment verificaiton and configuration scripts in progress

--- tidy3d/web/cli/develop.py
+++ tidy3d/web/cli/develop.py
@@ -1,6 +1,34 @@
 """Console script for tidy3d."""
-import sys
 import click
+import subprocess
+
+
+def verify_pipx_is_installed():
+    try:
+        # Running 'pipx --version' command
+        result = subprocess.run(["pipx", "--version"], capture_output=True, text=True, check=True)
+
+        # If the command was successful, it means pipx is installed
+        if result.returncode == 0:
+            print("pipx is installed: " + result.stdout)
+            return True
+    except:
+        # This exception is raised if the command returned a non-zero exit status
+        print("pipx is not installed or not found in the system PATH.")
+        return False
+
+
+def verify_poetry_is_installed():
+    try:
+        # Running 'poetry --version' command
+        result = subprocess.run(["poetry", "--version"], capture_output=True, text=True, check=True)
+        # If the command was successful, we'll get the version info
+        if result.returncode == 0:
+            print("Poetry is installed: " + result.stdout)
+            return True
+    except:
+        # This exception is raised if the command returned a non-zero exit status
+        raise OSError("Poetry is not installed or not found in the system PATH.")
 
 
 @click.group(name="develop")
@@ -9,46 +37,41 @@ def develop():
     pass
 
 
-@develop.command(name="configure-dev-environment", help="Verifies the development environment.")
-def configure_development_environment(args=None):
-    """Configure development environment."""
-    # Makes sure the package has installed all the development dependencies.
-    click.echo("Replace this message by putting your code into " "tidy3d.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
-    return 0
-
-
 @develop.command(name="verify-dev-environment", help="Verifies the development environment.")
 def verify_development_environment(args=None):
     # Does all the docs verifications
     # Checks all the other development dependencies are properly installed
     """Verifies that the environment in which this is run conforms to the provided poetry.lock with all the development options included."""
-    click.echo("Replace this message by putting your code into " "tidy3d.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
+    # Verify pipx is installed
+    verify_pipx_is_installed()
+    # Verify poetry is installed
+    verify_poetry_is_installed()
+    # Dry run the poetry install to understand the configuration
+    subprocess.run(["poetry", "install", "-E dev", "--dry-run"])
+    print(
+        "`poetry install -E dev` dry run on the `poetry.lock` complete.\nManually verify packages are properly installed."
+    )
     return 0
 
 
-@develop.command(
-    name="configure-docs-environment", help="Verifies the documentation configuration."
-)
-def configure_documentation_environment(args=None):
-    # Sets up the notebook submodule and updates it.
-    # Installs pandoc if required.
-    # Runs the documentation verificaiton script
-    """Verifies that the environment in which this is run conforms to the provided poetry.lock with all the development options included."""
-    click.echo("Replace this message by putting your code into " "tidy3d.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
-    return 0
+@develop.command(name="configure-dev-environment", help="Verifies the development environment.")
+def configure_development_environment(args=None):
+    """Configure development environment."""
+    # Verify and install pipx if required
+    try:
+        verify_pipx_is_installed()
+    except:
+        subprocess.run(["python -m pip install --user pipx"])
+        subprocess.run(["python -m -m pipx ensurepath"])
 
+    # Verify and install poetry if required
+    try:
+        verify_poetry_is_installed()
+    except:
+        subprocess.run(["pipx install poetry"])
 
-@develop.command(name="verify-docs-environment", help="Verifies the documentation configuration.")
-def verify_documentation_configuration(args=None):
-    # Checks pandoc version
-    # Checks documentation depednencies match
-    # Checks submodule is updated
-    """Verifies that the environment in which this is run conforms to the provided poetry.lock with all the development options included."""
-    click.echo("Replace this message by putting your code into " "tidy3d.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
+    # Makes sure the package has installed all the development dependencies.
+    subprocess.run(["poetry", "install", "-E dev"])
     return 0
 
 
