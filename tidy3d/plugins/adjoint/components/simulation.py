6f7a2a3f0660f8dd09017dd4d7dd895d8c1ba044
Tyler Hughes <tyler@flexcompute.com>
2023-12-05 18:56:38 -0800

    more refactoring

--- tidy3d/plugins/adjoint/components/simulation.py
+++ tidy3d/plugins/adjoint/components/simulation.py
@@ -376,163 +376,165 @@ class JaxSimulation(Simulation, JaxObject):
 
         return sim, jax_info
 
-    # def plot(
-    #     self,
-    #     x: float = None,
-    #     y: float = None,
-    #     z: float = None,
-    #     ax: Ax = None,
-    #     source_alpha: float = None,
-    #     monitor_alpha: float = None,
-    #     hlim: Tuple[float, float] = None,
-    #     vlim: Tuple[float, float] = None,
-    #     **patch_kwargs,
-    # ) -> Ax:
-    #     """Wrapper around regular :class:`.Simulation` structure plotting."""
-    #     sim, _ = self.to_simulation()
-    #     return sim.plot(
-    #         x=x,
-    #         y=y,
-    #         z=z,
-    #         ax=ax,
-    #         source_alpha=source_alpha,
-    #         monitor_alpha=monitor_alpha,
-    #         hlim=hlim,
-    #         vlim=vlim,
-    #         **patch_kwargs,
-    #     )
+    def plot(
+        self,
+        x: float = None,
+        y: float = None,
+        z: float = None,
+        ax: Ax = None,
+        source_alpha: float = None,
+        monitor_alpha: float = None,
+        hlim: Tuple[float, float] = None,
+        vlim: Tuple[float, float] = None,
+        **patch_kwargs,
+    ) -> Ax:
+        """Wrapper around regular :class:`.Simulation` structure plotting."""
+        sim, _ = self.to_simulation()
+        return sim.plot(
+            x=x,
+            y=y,
+            z=z,
+            ax=ax,
+            source_alpha=source_alpha,
+            monitor_alpha=monitor_alpha,
+            hlim=hlim,
+            vlim=vlim,
+            **patch_kwargs,
+        )
 
-    # def plot_eps(
-    #     self,
-    #     x: float = None,
-    #     y: float = None,
-    #     z: float = None,
-    #     freq: float = None,
-    #     alpha: float = None,
-    #     source_alpha: float = None,
-    #     monitor_alpha: float = None,
-    #     hlim: Tuple[float, float] = None,
-    #     vlim: Tuple[float, float] = None,
-    #     ax: Ax = None,
-    # ) -> Ax:
-    #     """Wrapper around regular :class:`.Simulation` permittivity plotting."""
-    #     sim, _ = self.to_simulation()
-    #     return sim.plot_eps(
-    #         x=x,
-    #         y=y,
-    #         z=z,
-    #         ax=ax,
-    #         source_alpha=source_alpha,
-    #         monitor_alpha=monitor_alpha,
-    #         hlim=hlim,
-    #         vlim=vlim,
-    #     )
+    def plot_eps(
+        self,
+        x: float = None,
+        y: float = None,
+        z: float = None,
+        freq: float = None,
+        alpha: float = None,
+        source_alpha: float = None,
+        monitor_alpha: float = None,
+        hlim: Tuple[float, float] = None,
+        vlim: Tuple[float, float] = None,
+        ax: Ax = None,
+    ) -> Ax:
+        """Wrapper around regular :class:`.Simulation` permittivity plotting."""
+        sim, _ = self.to_simulation()
+        return sim.plot_eps(
+            x=x,
+            y=y,
+            z=z,
+            ax=ax,
+            source_alpha=source_alpha,
+            monitor_alpha=monitor_alpha,
+            hlim=hlim,
+            vlim=vlim,
+        )
 
-    # def plot_structures(
-    #     self,
-    #     x: float = None,
-    #     y: float = None,
-    #     z: float = None,
-    #     ax: Ax = None,
-    #     hlim: Tuple[float, float] = None,
-    #     vlim: Tuple[float, float] = None,
-    # ) -> Ax:
-    #     """Plot each of simulation's structures on a plane defined by one nonzero x,y,z coordinate.
+    def plot_structures(
+        self,
+        x: float = None,
+        y: float = None,
+        z: float = None,
+        ax: Ax = None,
+        hlim: Tuple[float, float] = None,
+        vlim: Tuple[float, float] = None,
+    ) -> Ax:
+        """Plot each of simulation's structures on a plane defined by one nonzero x,y,z coordinate.
 
-    #     Parameters
-    #     ----------
-    #     x : float = None
-    #         position of plane in x direction, only one of x, y, z must be specified to define plane.
-    #     y : float = None
-    #         position of plane in y direction, only one of x, y, z must be specified to define plane.
-    #     z : float = None
-    #         position of plane in z direction, only one of x, y, z must be specified to define plane.
-    #     ax : matplotlib.axes._subplots.Axes = None
-    #         Matplotlib axes to plot on, if not specified, one is created.
-    #     hlim : Tuple[float, float] = None
-    #         The x range if plotting on xy or xz planes, y range if plotting on yz plane.
-    #     vlim : Tuple[float, float] = None
-    #         The z range if plotting on xz or yz planes, y plane if plotting on xy plane.
+        Parameters
+        ----------
+        x : float = None
+            position of plane in x direction, only one of x, y, z must be specified to define plane.
+        y : float = None
+            position of plane in y direction, only one of x, y, z must be specified to define plane.
+        z : float = None
+            position of plane in z direction, only one of x, y, z must be specified to define plane.
+        ax : matplotlib.axes._subplots.Axes = None
+            Matplotlib axes to plot on, if not specified, one is created.
+        hlim : Tuple[float, float] = None
+            The x range if plotting on xy or xz planes, y range if plotting on yz plane.
+        vlim : Tuple[float, float] = None
+            The z range if plotting on xz or yz planes, y plane if plotting on xy plane.
 
-    #     Returns
-    #     -------
-    #     matplotlib.axes._subplots.Axes
-    #         The supplied or created matplotlib axes.
-    #     """
-    #     sim, _ = self.to_simulation()
-    #     return sim.plot_structures(
-    #         x=x,
-    #         y=y,
-    #         z=z,
-    #         ax=ax,
-    #         hlim=hlim,
-    #         vlim=vlim,
-    #     )
+        Returns
+        -------
+        matplotlib.axes._subplots.Axes
+            The supplied or created matplotlib axes.
+        """
+        sim, _ = self.to_simulation()
+        return sim.plot_structures(
+            x=x,
+            y=y,
+            z=z,
+            ax=ax,
+            hlim=hlim,
+            vlim=vlim,
+        )
 
-    # def plot_structures_eps(
-    #     self,
-    #     x: float = None,
-    #     y: float = None,
-    #     z: float = None,
-    #     freq: float = None,
-    #     alpha: float = None,
-    #     cbar: bool = True,
-    #     reverse: bool = False,
-    #     ax: Ax = None,
-    #     hlim: Tuple[float, float] = None,
-    #     vlim: Tuple[float, float] = None,
-    # ) -> Ax:
-    #     """Plot each of simulation's structures on a plane defined by one nonzero x,y,z coordinate.
-    #     The permittivity is plotted in grayscale based on its value at the specified frequency.
+    def plot_structures_eps(
+        self,
+        x: float = None,
+        y: float = None,
+        z: float = None,
+        freq: float = None,
+        alpha: float = None,
+        cbar: bool = True,
+        reverse: bool = False,
+        ax: Ax = None,
+        hlim: Tuple[float, float] = None,
+        vlim: Tuple[float, float] = None,
+    ) -> Ax:
+        """Plot each of simulation's structures on a plane defined by one nonzero x,y,z coordinate.
+        The permittivity is plotted in grayscale based on its value at the specified frequency.
 
-    #     Parameters
-    #     ----------
-    #     x : float = None
-    #         position of plane in x direction, only one of x, y, z must be specified to define plane.
-    #     y : float = None
-    #         position of plane in y direction, only one of x, y, z must be specified to define plane.
-    #     z : float = None
-    #         position of plane in z direction, only one of x, y, z must be specified to define plane.
-    #     freq : float = None
-    #         Frequency to evaluate the relative permittivity of all mediums.
-    #         If not specified, evaluates at infinite frequency.
-    #     reverse : bool = False
-    #         If ``False``, the highest permittivity is plotted in black.
-    #         If ``True``, it is plotteed in white (suitable for black backgrounds).
-    #     cbar : bool = True
-    #         Whether to plot a colorbar for the relative permittivity.
-    #     alpha : float = None
-    #         Opacity of the structures being plotted.
-    #         Defaults to the structure default alpha.
-    #     ax : matplotlib.axes._subplots.Axes = None
-    #         Matplotlib axes to plot on, if not specified, one is created.
-    #     hlim : Tuple[float, float] = None
-    #         The x range if plotting on xy or xz planes, y range if plotting on yz plane.
-    #     vlim : Tuple[float, float] = None
-    #         The z range if plotting on xz or yz planes, y plane if plotting on xy plane.
+        Parameters
+        ----------
+        x : float = None
+            position of plane in x direction, only one of x, y, z must be specified to define plane.
+        y : float = None
+            position of plane in y direction, only one of x, y, z must be specified to define plane.
+        z : float = None
+            position of plane in z direction, only one of x, y, z must be specified to define plane.
+        freq : float = None
+            Frequency to evaluate the relative permittivity of all mediums.
+            If not specified, evaluates at infinite frequency.
+        reverse : bool = False
+            If ``False``, the highest permittivity is plotted in black.
+            If ``True``, it is plotteed in white (suitable for black backgrounds).
+        cbar : bool = True
+            Whether to plot a colorbar for the relative permittivity.
+        alpha : float = None
+            Opacity of the structures being plotted.
+            Defaults to the structure default alpha.
+        ax : matplotlib.axes._subplots.Axes = None
+            Matplotlib axes to plot on, if not specified, one is created.
+        hlim : Tuple[float, float] = None
+            The x range if plotting on xy or xz planes, y range if plotting on yz plane.
+        vlim : Tuple[float, float] = None
+            The z range if plotting on xz or yz planes, y plane if plotting on xy plane.
 
-    #     Returns
-    #     -------
-    #     matplotlib.axes._subplots.Axes
-    #         The supplied or created matplotlib axes.
-    #     """
-    #     sim, _ = self.to_simulation()
-    #     return sim.plot_structures_eps(
-    #         x=x,
-    #         y=y,
-    #         z=z,
-    #         freq=freq,
-    #         alpha=alpha,
-    #         cbar=cbar,
-    #         reverse=reverse,
-    #         ax=ax,
-    #         hlim=hlim,
-    #         vlim=vlim,
-    #     )
+        Returns
+        -------
+        matplotlib.axes._subplots.Axes
+            The supplied or created matplotlib axes.
+        """
+        sim, _ = self.to_simulation()
+        return sim.plot_structures_eps(
+            x=x,
+            y=y,
+            z=z,
+            freq=freq,
+            alpha=alpha,
+            cbar=cbar,
+            reverse=reverse,
+            ax=ax,
+            hlim=hlim,
+            vlim=vlim,
+        )
 
-    # def __eq__(self, other: JaxSimulation) -> bool:
-    #     """Are two JaxSimulation objects equal?"""
-    #     return self.to_simulation()[0] == other.to_simulation()[0]
+    def __eq__(self, other: JaxSimulation) -> bool:
+        """Are two JaxSimulation objects equal?"""
+        self_sim, self_jax_info = self.to_simulation()
+        other_sim, other_jax_info = other.to_simulation()
+        return self_sim == other_sim and self_jax_info == other_jax_info
 
     @classmethod
     def split_monitors(cls, monitors: List[Monitor], jax_info: JaxInfo) -> Dict[str, Monitor]:
