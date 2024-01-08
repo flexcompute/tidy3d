dfc784c8a2775c6dda3b9dd4c0169a85a523c2ad
momchil <momchil@flexcompute.com>
2024-01-08 13:48:48 -0800

    Black linting

--- metadata/description_length_checker.py
+++ metadata/description_length_checker.py
@@ -1,16 +1,16 @@
 import json
 
 # Specify the metadata file name
-metadata_file = 'metadata.txt'
+metadata_file = "metadata.txt"
 
 # Load the metadata dictionary from the text file
-with open(metadata_file, 'r', encoding='utf-8') as f:
+with open(metadata_file, "r", encoding="utf-8") as f:
     metadata_dict = json.loads(f.read())
 
 # For each item in the metadata dictionary
 for file_name, metadata in metadata_dict.items():
-    description = metadata.get('description', '')
+    description = metadata.get("description", "")
     description_length = len(description)
-    
-    if description_length>150 or description_length<70:
-        print(f'{file_name}: {description_length}')
+
+    if description_length > 150 or description_length < 70:
+        print(f"{file_name}: {description_length}")
--- metadata/update_notebook_metadata.py
+++ metadata/update_notebook_metadata.py
@@ -2,13 +2,13 @@ import os
 import json
 
 # Specify the directory path
-dir_path = os.path.join(os.path.dirname(__file__), '..')
+dir_path = os.path.join(os.path.dirname(__file__), "..")
 
 # Specify the metadata file name
-metadata_file = 'metadata.txt'
+metadata_file = "metadata.txt"
 
 # Load the updated metadata dictionary from the text file
-with open(metadata_file, 'r', encoding='utf-8') as f:
+with open(metadata_file, "r", encoding="utf-8") as f:
     updated_metadata = json.loads(f.read())
 
 # For each .ipynb file
@@ -16,15 +16,15 @@ for file_name, metadata in updated_metadata.items():
     # Load the notebook
     notebook_file = os.path.join(dir_path, file_name)
     if os.path.exists(notebook_file):
-        with open(notebook_file, 'r', encoding='utf-8') as f:
+        with open(notebook_file, "r", encoding="utf-8") as f:
             notebook = json.load(f)
 
         # Update the metadata
-        notebook['metadata']['title'] = metadata['title']
-        notebook['metadata']['description'] = metadata['description']
-        notebook['metadata']['keywords'] = metadata['keywords']
-        notebook['metadata']['feature_image'] = metadata['feature_image']
+        notebook["metadata"]["title"] = metadata["title"]
+        notebook["metadata"]["description"] = metadata["description"]
+        notebook["metadata"]["keywords"] = metadata["keywords"]
+        notebook["metadata"]["feature_image"] = metadata["feature_image"]
 
         # Save the updated notebook
-        with open(notebook_file, 'w', encoding='utf-8') as f:
+        with open(notebook_file, "w", encoding="utf-8") as f:
             json.dump(notebook, f, indent=2)
