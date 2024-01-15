aefed35ea4357a1b00cee265aff5d26f6e248c22
Tyler Hughes <tyler@flexcompute.com>
2024-01-15 11:24:45 -0500

    DataArray.to_hdf5 accepts path and group

--- tests/test_components/test_IO.py
+++ tests/test_components/test_IO.py
@@ -6,7 +6,7 @@ import pytest
 import numpy as np
 from time import time
 import dill as pickle
-
+import h5py
 
 from tidy3d import __version__
 import tidy3d as td
@@ -316,3 +316,18 @@ def test_monitor_data_from_file():
     mode_data = td.SimulationData.mnt_data_from_file(fname, mnt_name="mode")
     assert isinstance(mode_data, td.ModeData)
     assert mode_data.monitor == sim.monitors[1]
+
+
+def test_data_array_to_hdf5(tmp_path):
+    values = np.linspace(0, 1, 10)
+    coords = dict(f=values)
+    flux = td.FluxDataArray(values, coords=coords)
+
+    path = str(tmp_path / "flux.hdf5")
+
+    # pass a file handle
+    with h5py.File(path, "w") as f_handle:
+        flux.to_hdf5(fname=f_handle, group_path="test")
+
+    # pass a file name
+    flux.to_hdf5(fname=path, group_path="test")
