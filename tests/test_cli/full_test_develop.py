227c7e84c885eb41107156eec2a0e980969763ed
daquintero <dario@flexcompute.com>
2024-01-22 17:47:07 +0100

    TEST: Starting to include cli tests

--- tests/test_cli/full_test_develop.py
+++ tests/test_cli/full_test_develop.py
@@ -2,8 +2,10 @@
 These scripts just test the CLI commands for the develop command, and verify that they run properly.
 """
 import pytest
+import os
 from click.testing import CliRunner
 from tidy3d.web.cli import tidy3d_cli
+from unittest.mock import patch
 
 
 @pytest.fixture
@@ -40,3 +42,34 @@ def test_develop_build_docs(runner):
 
         result = runner.invoke(tidy3d_cli, ["develop", "build-docs"])
         assert result.exit_code == 0  # Verifies docs build successfully
+
+
+def test_replace_in_files(runner):
+    import json
+
+    with runner.isolated_filesystem():
+        os.mkdir("test_directory")
+        with open("test_directory/test_file.txt", "w") as f:
+            f.write("Some content with tidy3d.someuniquestringa")
+
+        with open("replace_dict.json", "w") as f:
+            json.dump({"0.18.0": {"tidy3d.someuniquestringa": "tidy3d.someuniquestring2"}}, f)
+
+        with patch("builtins.input", return_value="y"):
+            result = runner.invoke(
+                tidy3d_cli,
+                [
+                    "develop",
+                    "replace-in-files",
+                    "--directory",
+                    "test_directory",
+                    "--json-dictionary",
+                    "replace_dict.json",
+                    "--selected-version",
+                    "0.18.0",
+                    "--dry-run",
+                    True,
+                ],
+            )
+
+        assert result.exit_code == 0
