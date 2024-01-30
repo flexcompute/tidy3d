7e26108584c07846994a9bffe021000b1a06f2d0
qingeng <qingeng>
2024-01-30 14:00:08 +0800

    Update the minimum tls version for python client to avoid https shake verify issue.

--- tests/test_web/test_env.py
+++ tests/test_web/test_env.py
@@ -1,3 +1,4 @@
+import ssl
 from tidy3d.web.core.environment import Env
 
 
@@ -7,3 +8,17 @@ def test_tidy3d_env():
 
     Env.enable_caching(False)
     assert Env.current.enable_caching == False
+
+
+def test_set_ssl_version():
+    Env.set_ssl_version(ssl.TLSVersion.TLSv1_3)
+    assert Env.current.ssl_version == ssl.TLSVersion.TLSv1_3
+
+    Env.set_ssl_version(ssl.TLSVersion.TLSv1_2)
+    assert Env.current.ssl_version == ssl.TLSVersion.TLSv1_2
+
+    Env.set_ssl_version(ssl.TLSVersion.TLSv1_1)
+    assert Env.current.ssl_version == ssl.TLSVersion.TLSv1_1
+
+    Env.set_ssl_version(None)
+    assert Env.current.ssl_version is None
