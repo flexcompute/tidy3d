4cda5fff1ca4fb95ee9c477ced5a46d67eee5c6d
daquintero <dario@flexcompute.com>
2024-02-13 17:48:30 +0100

    FEAT: Improved testing develop commands

--- tidy3d/web/cli/develop/tests.py
+++ tidy3d/web/cli/develop/tests.py
@@ -4,6 +4,7 @@ notebooks in order to achieve reproducibility between hardwares.
 """
 
 import click
+from typing import Literal, Optional
 from .utils import echo_and_run_subprocess
 from .index import develop
 from .install import install_in_poetry
@@ -13,29 +14,54 @@ __all__ = [
     "test_in_environment_command",
 ]
 
+section_test_options = Literal["base", "notebooks"]
+option_test_types = Literal["execute_all"]
 
-def test_options(options: list):
+
+def test_options(section: section_test_options, options: Optional[list] = None):
     """
     Inclusive rather than exclusive tests in a given set of environments.
 
     Parameters
     ----------
-    options : list
-        A list of options for which tests to run. Options are 'base' and 'notebooks'.
+    section : Literal["base", "notebooks"]
+        The section of tests to run.
+
+    options : Optional[list]
+        A list of options for which tests to run. Defaults to None.
+
+    Raises
+    ------
+    ValueError
+        If an invalid option is passed for the section.
     """
-    if "base" in options:
+    if "base" == section:
         echo_and_run_subprocess(["poetry", "run", "pytest", "-rA", "tests"])
-    if "notebooks" in options:
-        echo_and_run_subprocess(
-            ["poetry", "run", "pytest", "-rA", "tests/docs/notebooks/full_test_notebooks.py"]
-        )
+    if "notebooks" == section:
+        if "execute_all" in options:
+            echo_and_run_subprocess(
+                ["poetry", "run", "pytest", "-rA", "tests/docs/notebooks/execute_all_notebooks.py"]
+            )
+        else:
+            raise ValueError(
+                "Invalid option for 'notebooks' section. Valid options are 'execute_all'."
+            )
 
 
+@develop.command(name="test", help="Installs the specified poetry environment and tests")
 @click.option(
-    "--types",
-    default=["base"],
+    "-s",
+    "--section",
+    default="base",
     help="Types of tests to run. Defaults to 'base'. Other options",
-    type=list,
+    type=str,
+)
+@click.option(
+    "-o",
+    "--options",
+    default=None,
+    help="Options to configure the section tests, a list of options. Defaults to None.",
+    type=option_test_types,
 )
 @click.option(
     "--env",
@@ -43,10 +69,9 @@ def test_options(options: list):
     help="Poetry environment to install. Defaults to 'dev'.",
     type=str,
 )
-@develop.command(
-    name="test-in-envrionment", help="Installs the specified poetry environment and tests"
-)
-def test_in_environment_command(types: list, env: str = "dev"):
+def test_in_environment_command(
+    section: section_test_options, options: option_test_types, env: str
+):
     """
     Installs a poetry environment specified by the extra definition in pyproject.toml and runs tests with pytest and
     any additional arguments. Requires a poetry installation so make sure to verify the installation previously.
@@ -61,5 +86,5 @@ def test_in_environment_command(types: list, env: str = "dev"):
     env : str
         The name of the poetry environment to install. Defaults to 'dev'. See pyproject.toml
     """
-    install_in_poetry(env)
-    test_options(types)
+    install_in_poetry(env=env)
+    test_options(section=section, options=options)
