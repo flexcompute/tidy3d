2201ed84f1084dc4233ae98f445607eaaa8ca92e
Yannick Augenstein <yannick@flexcompute.com>
2024-06-17 17:22:53 +0200

    Clarify docstrings, refactoring

--- tidy3d/plugins/autograd/functions.py
+++ tidy3d/plugins/autograd/functions.py
@@ -663,6 +663,32 @@ def threshold(
     return np.where(array < level, vmin, vmax)
 
 
+def _evaluate_nearest(
+    indices: NDArray[np.int64], norm_distances: NDArray[np.float64], values: NDArray[np.float64]
+) -> NDArray[np.float64]:
+    idx_res = tuple(np.where(yi <= 0.5, i, i + 1) for i, yi in zip(indices, norm_distances))
+    return values[idx_res]
+
+
+def _evaluate_linear(
+    indices: NDArray[np.int64], norm_distances: NDArray[np.float64], values: NDArray[np.float64]
+) -> NDArray[np.float64]:
+    _slice = (slice(None),) + (None,) * (values.ndim - len(indices))
+
+    ix = zip(indices, (1 - yi for yi in norm_distances))
+    iy = zip((i + 1 for i in indices), norm_distances)
+
+    value = np.zeros(1)
+    for h in itertools.product(*zip(ix, iy)):
+        edge_indices, weights = zip(*h)
+        weight = np.ones(1)
+        for w in weights:
+            weight = weight * w
+        term = np.array(values[edge_indices]) * weight[_slice]
+        value = value + term
+    return value
+
+
 def interpn(
     points: tuple[NDArray[np.float64], ...],
     values: NDArray[np.float64],
@@ -670,16 +696,16 @@ def interpn(
     *,
     method: InterpolationType = "linear",
 ) -> NDArray[np.float64]:
-    """Interpolate over a regular grid in arbitrary dimensions.
+    """Interpolate over a rectilinear grid in arbitrary dimensions.
 
-    This function has the same interface as `scipy.interpolate.interpn` but is differentiable with autograd.
+    This function mirrors the interface of `scipy.interpolate.interpn` but is differentiable with autograd.
 
     Parameters
     ----------
     points : tuple[NDArray[np.float64], ...]
-        The points defining the regular grid in n dimensions.
+        The points defining the rectilinear grid in n dimensions.
     values : NDArray[np.float64]
-        The data values on the regular grid.
+        The data values on the rectilinear grid.
     xi : tuple[NDArray[np.float64], ...]
         The coordinates to sample the gridded data at.
     method : InterpolationType, optional
@@ -697,7 +723,7 @@ def interpn(
 
     See Also
     --------
-    scipy.interpolate.interpn : Interpolation on a regular grid in arbitrary dimensions.
+    scipy.interpolate.interpn : Interpolation on a rectilinear grid in arbitrary dimensions.
         https://docs.scipy.org/doc/scipy/reference/generated/scipy.interpolate.interpn.html
     """
     if method == "nearest":
@@ -715,61 +741,3 @@ def interpn(
     result = interp_fn(indices, norm_distances, values)
     result = np.where(nans, np.nan, result)
     return np.reshape(result, shape[:-1] + values.shape[ndim:])
-
-
-def _evaluate_nearest(
-    indices: NDArray[np.int64], norm_distances: NDArray[np.float64], values: NDArray[np.float64]
-) -> NDArray[np.float64]:
-    """Evaluate the nearest neighbor interpolation.
-
-    Parameters
-    ----------
-    indices : NDArray[np.int64]
-        The indices of the grid points.
-    norm_distances : NDArray[np.float64]
-        The normalized distances to the grid points.
-    values : NDArray[np.float64]
-        The data values on the regular grid.
-
-    Returns
-    -------
-    NDArray[np.float64]
-        The interpolated values.
-    """
-    idx_res = tuple(np.where(yi <= 0.5, i, i + 1) for i, yi in zip(indices, norm_distances))
-    return values[idx_res]
-
-
-def _evaluate_linear(
-    indices: NDArray[np.int64], norm_distances: NDArray[np.float64], values: NDArray[np.float64]
-) -> NDArray[np.float64]:
-    """Evaluate the linear interpolation.
-
-    Parameters
-    ----------
-    indices : NDArray[np.int64]
-        The indices of the grid points.
-    norm_distances : NDArray[np.float64]
-        The normalized distances to the grid points.
-    values : NDArray[np.float64]
-        The data values on the regular grid.
-
-    Returns
-    -------
-    NDArray[np.float64]
-        The interpolated values.
-    """
-    _slice = (slice(None),) + (None,) * (values.ndim - len(indices))
-
-    ix = zip(indices, (1 - yi for yi in norm_distances))
-    iy = zip((i + 1 for i in indices), norm_distances)
-
-    value = np.zeros(1)
-    for h in itertools.product(*zip(ix, iy)):
-        edge_indices, weights = zip(*h)
-        weight = np.ones(1)
-        for w in weights:
-            weight = weight * w
-        term = np.array(values[edge_indices]) * weight[_slice]
-        value = value + term
-    return value
