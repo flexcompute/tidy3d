d5cd8b434b60dca7cbcf4f0fb619c6e2a83e006c
Marc Bolinches <marc@flexcompute.com>
2025-01-10 15:56:02 +0100

    Test no_vtk

--- tests/_test_data/_test_datasets_no_vtk.py
+++ tests/_test_data/_test_datasets_no_vtk.py
@@ -21,11 +21,10 @@ def hide_vtk(monkeypatch, request):
 
 
 @pytest.mark.usefixtures("hide_vtk")
-@pytest.mark.parametrize("dataset_type_ind", [0, 1])
-def test_triangular_dataset_no_vtk(tmp_path, log_capture, dataset_type_ind):
-    _test_triangular_dataset(log_capture, tmp_path, "test_name", dataset_type_ind, no_vtk=True)
+def test_triangular_dataset_no_vtk(tmp_path, log_capture):
+    _test_triangular_dataset(log_capture, tmp_path, "test_name", 0, no_vtk=True)
 
 
 @pytest.mark.usefixtures("hide_vtk")
-def test_tetrahedral_dataset_no_vtk(tmp_path, log_capture, dataset_type_ind):
-    _test_tetrahedral_dataset(log_capture, tmp_path, "test_name", dataset_type_ind, no_vtk=True)
+def test_tetrahedral_dataset_no_vtk(tmp_path, log_capture):
+    _test_tetrahedral_dataset(log_capture, tmp_path, "test_name", 0, no_vtk=True)
