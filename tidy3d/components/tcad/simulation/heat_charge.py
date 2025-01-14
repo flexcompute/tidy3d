e691ea230c7217c288d6496e916e874f38175817
daquinteroflex <dario@flexcompute.com>
2025-01-14 19:13:28 +0100

    fix

--- tidy3d/components/tcad/simulation/heat_charge.py
+++ tidy3d/components/tcad/simulation/heat_charge.py
@@ -79,7 +79,7 @@ from tidy3d.components.tcad.viz import (
     plot_params_heat_bc,
     plot_params_heat_source,
 )
-from tidy3d.components.types import TYPE_TAG_STR, Ax, Bound, ScalarSymmetry, Shapely
+from tidy3d.components.types import TYPE_TAG_STR, Ax, Bound, ScalarSymmetry, Shapely, annotate_type
 from tidy3d.components.viz import PlotParams, add_ax_if_none, equal_aspect
 from tidy3d.constants import VOLUMETRIC_HEAT_RATE, inf
 from tidy3d.exceptions import SetupError
@@ -271,11 +271,13 @@ class HeatChargeSimulation(AbstractSimulation):
         (), title="Monitors", description="Monitors in the simulation.", discriminator=TYPE_TAG_STR
     )
 
-    boundary_spec: Tuple[annotate_type(Union[HeatChargeBoundarySpec, HeatBoundarySpec]), ...] = pd.Field(
-        (),
-        title="Boundary Condition Specifications",
-        description="List of boundary condition specifications.",
-        discriminator=TYPE_TAG_STR,
+    boundary_spec: Tuple[annotate_type(Union[HeatChargeBoundarySpec, HeatBoundarySpec]), ...] = (
+        pd.Field(
+            (),
+            title="Boundary Condition Specifications",
+            description="List of boundary condition specifications.",
+            discriminator=TYPE_TAG_STR,
+        )
     )
     # NOTE: creating a union with HeatBoundarySpec for backwards compatibility
 
