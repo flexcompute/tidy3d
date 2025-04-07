fix[adjoint]: remove frequency summing in polyslab gradient calculation

tidy3d/components/geometry/base.py
@@ -2554,8 +2554,10 @@ class Box(SimplePlaneIntersection, Centered):
         fld_normal, flds_perp = self.pop_axis(("Ex", "Ey", "Ez"), axis=axis_normal)
 
         # normal and tangential fields
-        D_normal = derivative_info.D_der_map[fld_normal]
-        Es_perp = tuple(derivative_info.E_der_map[key] for key in flds_perp)
+        D_normal = derivative_info.D_der_map[fld_normal].sel(f=derivative_info.frequency)
+        Es_perp = tuple(
+            derivative_info.E_der_map[key].sel(f=derivative_info.frequency) for key in flds_perp
+        )
 
         # normal and tangential bounds
         bounds_T = np.array(derivative_info.bounds).T  # put (xyz) first dimension
@@ -2580,7 +2582,10 @@ class Box(SimplePlaneIntersection, Centered):
             return 0.0
 
         # grab permittivity data inside and outside edge in normal direction
-        eps_xyz = [derivative_info.eps_data[f"eps_{dim}{dim}"] for dim in "xyz"]
+        eps_xyz = [
+            derivative_info.eps_data[f"eps_{dim}{dim}"].sel(f=derivative_info.frequency)
+            for dim in "xyz"
+        ]
 
         # number of cells from the edge of data to register "inside" (index = num_cells_in - 1)
         num_cells_in = 4
@@ -2621,7 +2626,7 @@ class Box(SimplePlaneIntersection, Centered):
                 bounds=bounds_perp,
             )
 
-            return complex(integral_result.sum(dim="f"))
+            return complex(integral_result)
 
         # put together VJP using D_normal and E_perp integration
         vjp_value = 0.0