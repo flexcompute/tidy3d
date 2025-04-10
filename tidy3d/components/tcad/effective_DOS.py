c267f478f6508e7fd96fdc0274434a82c20f59de
Vasily Zabelin <vasily.zabelin@flexcompute.com>
2025-04-10 10:11:54 -0700

    Add effective DOS classes for the Charge solver

--- tidy3d/components/tcad/effective_DOS.py
+++ tidy3d/components/tcad/effective_DOS.py
@@ -4,14 +4,17 @@ import numpy as np
 import pydantic.v1 as pd
 
 from tidy3d.components.base import Tidy3dBaseModel
-from tidy3d.constants import HBAR
 
 from ...exceptions import DataError
 
 # constants definition
 k_B = 1.380649e-23  # Boltzmann constant in J/K
 m_e = 9.1093837139e-31  # electron mass in kg
-DOS_aux_const = 2.0 * np.power((m_e * k_B) / (2 * np.pi * HBAR), 1.5)
+Planck_hbar = 1.054571817e-34  # reduced Planck constant in J*s
+m_3_to_cm_3 = 1e-6  # conversion factor from m^(-3) to cm^(-3)
+DOS_aux_const = (
+    2.0 * np.power((m_e * k_B) / (2 * np.pi * Planck_hbar * Planck_hbar), 1.5) * m_3_to_cm_3
+)
 
 
 class EffectiveDOS(Tidy3dBaseModel, ABC):
@@ -22,6 +25,11 @@ class EffectiveDOS(Tidy3dBaseModel, ABC):
         """Abstract method to calculate the effective density of states."""
         pass
 
+    @abstractmethod
+    def _calc_eff_DOS_derivative(self, T: float):
+        """Abstract method to calculate the temperature derivative of the effective density of states."""
+        pass
+
     def get_effective_DOS(self, T: float):
         if T <= 0:
             raise DataError(
@@ -29,6 +37,14 @@ class EffectiveDOS(Tidy3dBaseModel, ABC):
             )
 
         return self._calc_eff_DOS(T)
+
+    def get_effective_DOS_derivative(self, T: float):
+        if T <= 0:
+            raise DataError(
+                f"Incorrect temperature value ({T}) for the effectve density of states calculation."
+            )
+
+        return self._calc_eff_DOS_derivative(T)
 
 
 class ConstantEffectiveDOS(EffectiveDOS):
@@ -40,6 +56,9 @@ class ConstantEffectiveDOS(EffectiveDOS):
 
     def _calc_eff_DOS(self, T: float):
         return self.N
+
+    def _calc_eff_DOS_derivative(self, T: float):
+        return 0.0
 
 
 class IsotropicEffectiveDOS(EffectiveDOS):
@@ -62,6 +81,9 @@ class IsotropicEffectiveDOS(EffectiveDOS):
 
     def _calc_eff_DOS(self, T: float):
         return np.power(self.m_eff * T, 1.5) * DOS_aux_const
+
+    def _calc_eff_DOS_derivative(self, T: float):
+        return self._calc_eff_DOS(T) * 1.5 / T
 
 
 class MultiValleyEffectiveDOS(EffectiveDOS):
@@ -101,6 +123,9 @@ class MultiValleyEffectiveDOS(EffectiveDOS):
             * DOS_aux_const
         )
 
+    def _calc_eff_DOS_derivative(self, T: float):
+        return self._calc_eff_DOS(T) * 1.5 / T
+
 
 class DualValleyEffectiveDOS(EffectiveDOS):
     """Effective density of states model that assumes combibation of light holes and heavy holes with isotropic effective masses.
@@ -129,3 +154,6 @@ class DualValleyEffectiveDOS(EffectiveDOS):
 
     def _calc_eff_DOS(self, T: float):
         return (np.power(self.m_eff_lh * T, 1.5) + np.power(self.m_eff_hh * T, 1.5)) * DOS_aux_const
+
+    def _calc_eff_DOS_derivative(self, T: float):
+        return self._calc_eff_DOS(T) * 1.5 / T
