864c20f5d9e9a391679a3f96284ff9d229784440
daquinteroflex <dario@flexcompute.com>
2025-04-28 17:28:45 +0200

    Update svg

--- docs/conf.py
+++ docs/conf.py
@@ -147,7 +147,7 @@ html_title = "Tidy3D Electromagnetic Solver"
 html_theme_options = {
     "logo": {
         "image_light": "./_static/img/Tidy3D-logo.svg",
-        "image_dark": "./_static/img/Tidy3D-logo-white.png",
+        "image_dark": "./_static/img/Tidy3D-logo-white.svg",
     },
     "path_to_docs": "docs",
     "repository_url": "https://github.com/flexcompute/tidy3d",
