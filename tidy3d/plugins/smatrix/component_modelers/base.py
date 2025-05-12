ce4d2d160ed2f8379fc38a7fa5a20d4243a66cdb
daquinteroflex <dario@flexcompute.com>
2025-05-12 15:59:07 +0200

    smatrix validator warning

--- tidy3d/plugins/smatrix/component_modelers/base.py
+++ tidy3d/plugins/smatrix/component_modelers/base.py
@@ -313,5 +313,6 @@ class AbstractComponentModeler(ABC, Tidy3dBaseModel):
     def _warn_rf_license(cls, values):
         warnings.warn(
             f"ℹ️ ⚠️ RF simulations are subject to new license requirements in the future. You are using the RF-specific component: {cls}.",
+            FutureWarning,
         )
         return values
--- tidy3d/plugins/smatrix/data/terminal.py
+++ tidy3d/plugins/smatrix/data/terminal.py
@@ -30,7 +30,8 @@ class PortDataArray(DataArray):
     @pd.root_validator(pre=False)
     def _warn_rf_license(cls, values):
         warnings.warn(
-            f"ℹ️ ⚠️ RF simulations are subject to new license requirements in the future. You are using the RF-specific component: {cls}."
+            f"ℹ️ ⚠️ RF simulations are subject to new license requirements in the future. You are using the RF-specific component: {cls}.",
+            FutureWarning,
         )
         return values
 
@@ -56,6 +57,7 @@ class TerminalPortDataArray(DataArray):
     @pd.root_validator(pre=False)
     def _warn_rf_license(cls, values):
         warnings.warn(
-            f"ℹ️ ⚠️ RF simulations are subject to new license requirements in the future. You are using the RF-specific component: {cls}."
+            f"ℹ️ ⚠️ RF simulations are subject to new license requirements in the future. You are using the RF-specific component: {cls}.",
+            FutureWarning,
         )
         return values
