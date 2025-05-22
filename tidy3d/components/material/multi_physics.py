6fa68002b0a499da5895c8912ae18f9057b42737
Frederik Schubert <frederik.schubert@flexcompute.com>
2025-05-22 16:15:55 +0200

    add threshold to changed files report as well

--- tidy3d/components/material/multi_physics.py
+++ tidy3d/components/material/multi_physics.py
@@ -158,8 +158,6 @@ class MultiPhysicsMedium(Tidy3dBaseModel):
             "Did you mean to access the attribute of one of the optical, heat or charge media?"
         )
 
-        print("TMP UNREACHABLE")
-
     @property
     def heat_spec(self):
         if self.heat is not None:
