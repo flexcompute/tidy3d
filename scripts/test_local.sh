cf8de866b44c65601cfeb853707cd7bc3f1d1ea8
Frederik Schubert <frederik.schubert@flexcompute.com>
2025-05-27 11:44:43 +0200

    move checks to local test script and add manual pre-commit hook

--- scripts/test_local.sh
+++ scripts/test_local.sh
@@ -7,7 +7,10 @@ ruff format scripts/ --check --diff
 
 ruff check tidy3d --diff
 
-pytest -rA tests/
+pytest -rA --cov tests/
 # to test without vtk, one has to restart pytest
 pytest -rA tests/_test_data/_test_datasets_no_vtk.py
 pytest --doctest-modules tidy3d/ docs/
+coverage xml
+diff-cover --compare-branch=origin/develop coverage.xml
+
