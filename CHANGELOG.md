4a5add080dfc9cfa4be628515c923c97e1a9324e
saeedece <git@saeedc.com>
2025-07-06 15:12:21 -0400

    Feat: Add `PointDipole.from_angles()` (#2623)
    * Feat: Add `PointDipole.from_angles()`
    
    Class method that returns a list of `PointDipole` objects used to emulate a single dipole polarized in an arbitrary direction. The direction is specificed using a polar and azimuthal angle.
    
    * Fix: Pass polarization argument as string + NumPy-style docstring.
    
    * Test: Simple testing function for `PointDipole.from_angles()`.
    
    * Refc: Rename `PointDipole.from_angles()` to `PointDipole.sources_from_angles()`
    
    ---------
    
    Co-authored-by: momchil-flex <92756559+momchil-flex@users.noreply.github.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 - Add support for `np.unwrap` in `tidy3d.plugins.autograd`.
 - Add Nunley variant to germanium material library based on Nunley et al. 2016 data.
+- Add `PointDipole.sources_from_angles()` that constructs a list of `PointDipole` objects needed to emulate a dipole oriented at a user-provided set of polar and azimuthal angles.
 - Added `priority` parameter to `web.run()` and related functions to allow vGPU users to set task priority (1-10) in the queue.
 
 ### Changed
--- tidy3d/components/source/current.py
+++ tidy3d/components/source/current.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 from abc import ABC
+from math import cos, isclose, sin
 from typing import Optional
 
 import pydantic.v1 as pydantic
@@ -16,6 +17,7 @@ from tidy3d.components.validators import assert_single_freq_in_range, warn_if_da
 from tidy3d.constants import MICROMETER
 
 from .base import Source
+from .time import SourceTimeType
 
 
 class CurrentSource(Source, ABC):
@@ -106,6 +108,62 @@ class PointDipole(CurrentSource, ReverseInterpolatedSource):
         description="Size in x, y, and z directions, constrained to ``(0, 0, 0)``.",
         units=MICROMETER,
     )
+
+    @classmethod
+    def sources_from_angles(
+        cls,
+        source_time: SourceTimeType,
+        angle_theta: float,
+        angle_phi: float,
+        component: Literal["electric", "magnetic"] = "electric",
+        **kwargs,
+    ) -> list[PointDipole]:
+        """Returns a list of `PointDipole` objects used to emulate a single dipole polarized in an arbitrary direction. The direction is specificed using a polar and azimuthal angle.
+
+        Parameters
+        ----------
+        source_time: :class:`.SourceTime`
+            Specification of the source time-dependence.
+        angle_theta : float
+            Polar angle w.r.t. the z-axis.
+        angle_phi : float
+            Azimuth angle around the z-axis.
+        component : Literal["electric", "magnetic"] = "electric"
+            The type of polarization.
+        kwargs : dict
+            Keyword arguments passed to ``PointDipole()``, excluding ``source_time`` and ``polarization``
+
+        Returns
+        -------
+        list[PointDipole]
+            A list of ``PointDipole`` objects that emulate a single dipole with an arbitrary direction of polarization.
+        """
+        if not (component == "electric" or component == "magnetic"):
+            raise ValueError('Component must be "electric" or "magnetic"')
+
+        dipoles: list[PointDipole] = []
+        polarizations = ["Ex", "Ey", "Ez"] if component == "electric" else ["Hx", "Hy", "Hz"]
+
+        multipliers = [
+            sin(angle_theta) * cos(angle_phi),
+            sin(angle_theta) * sin(angle_phi),
+            cos(angle_theta),
+        ]
+
+        for polarization, mult in zip(polarizations, multipliers):
+            if not isclose(mult, 0.0, rel_tol=0.0, abs_tol=1e-9):
+                modulated_source_time = source_time.updated_copy(
+                    amplitude=source_time.amplitude * mult
+                )
+                dipoles.append(
+                    cls(
+                        source_time=modulated_source_time,
+                        polarization=polarization,
+                        **kwargs,
+                    )
+                )
+
+        return dipoles
 
 
 class CustomCurrentSource(ReverseInterpolatedSource):
