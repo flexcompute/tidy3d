6ac58c51957822622066e0deefdb0d0950315e90
dmarek <damian@flexcompute.com>
2025-08-07 16:05:51 -0400

    fix monkeypatch and anticipate some changes

--- tests/test_plugins/smatrix/test_terminal_component_modeler.py
+++ tests/test_plugins/smatrix/test_terminal_component_modeler.py
@@ -16,7 +16,6 @@ from tidy3d.plugins.microwave import (
     VoltageIntegralAxisAligned,
 )
 from tidy3d.plugins.smatrix import (
-    AbstractComponentModeler,
     CoaxialLumpedPort,
     LumpedPort,
     PortDataArray,
@@ -44,7 +43,9 @@ def run_component_modeler(
         data=list(batch_data.values()),
     )
     modeler_data = TerminalComponentModelerData(modeler=modeler, data=port_data)
-    monkeypatch.setattr(AbstractComponentModeler, "inv", lambda matrix: np.eye(len(modeler.ports)))
+    monkeypatch.setattr(
+        td.plugins.smatrix.utils, "port_array_inv", lambda matrix: np.eye(len(modeler.ports))
+    )
     monkeypatch.setattr(
         td.plugins.smatrix.utils,
         "compute_F",
