daabb73bb78e7ceeed2dfa89b0051f76649eedf3
George-Guryev-flxcmp <georgy.guryev@flexcompute.com>
2025-08-07 18:49:23 -0400

    Add taper support to AntennaArrayCalculator and refactor array factor calculation
    - Implemented taper integration to allow amplitude weighting in antenna arrays
    - Refactored array factor computation for improved clarity and efficiency
    - Updated relevant tests and documentation accordingly

--- tidy3d/plugins/microwave/__init__.py
+++ tidy3d/plugins/microwave/__init__.py
@@ -4,7 +4,16 @@ from __future__ import annotations
 
 from . import models
 from .array_factor import (
+    BlackmanHarrisWindow,
+    BlackmanWindow,
+    ChebWindow,
+    HammingWindow,
+    HannWindow,
+    KaiserWindow,
+    RadialTaper,
     RectangularAntennaArrayCalculator,
+    RectangularTaper,
+    TaylorWindow,
 )
 from .auto_path_integrals import path_integrals_from_lumped_element
 from .custom_path_integrals import (
@@ -23,14 +32,23 @@ from .rf_material_library import rf_material_library
 
 __all__ = [
     "AxisAlignedPathIntegral",
+    "BlackmanHarrisWindow",
+    "BlackmanWindow",
+    "ChebWindow",
     "CurrentIntegralAxisAligned",
     "CurrentIntegralTypes",
     "CustomCurrentIntegral2D",
     "CustomPathIntegral2D",
     "CustomVoltageIntegral2D",
+    "HammingWindow",
+    "HannWindow",
     "ImpedanceCalculator",
+    "KaiserWindow",
     "LobeMeasurer",
+    "RadialTaper",
     "RectangularAntennaArrayCalculator",
+    "RectangularTaper",
+    "TaylorWindow",
     "VoltageIntegralAxisAligned",
     "VoltageIntegralTypes",
     "models",
