fix[microwave]: fix column vs row-major order in antenna_amps and taper for consistent amplitude scaling

tidy3d/plugins/microwave/array_factor.py
@@ -789,7 +789,7 @@ class RectangularAntennaArrayCalculator(AbstractAntennaArrayCalculator):
         y = (np.arange(self.array_size[1]) - (self.array_size[1] - 1) / 2) * self.spacings[1]
         z = (np.arange(self.array_size[2]) - (self.array_size[2] - 1) / 2) * self.spacings[2]
 
-        X, Y, Z = np.meshgrid(x, y, z)
+        X, Y, Z = np.meshgrid(x, y, z, indexing="ij")
 
         return np.transpose([X.ravel(), Y.ravel(), Z.ravel()])
 
@@ -810,6 +810,7 @@ class RectangularAntennaArrayCalculator(AbstractAntennaArrayCalculator):
 
             else:
                 amps = self.taper.amp_multipliers(self.array_size)
+
             return np.ravel(amps)
 
         amps_per_dim = [
@@ -829,7 +830,7 @@ class RectangularAntennaArrayCalculator(AbstractAntennaArrayCalculator):
             np.arange(self.array_size[dim]) * self.phase_shifts[dim] for dim in range(3)
         ]
 
-        phase_shifts_grid = np.meshgrid(*phase_shifts_per_dim)
+        phase_shifts_grid = np.meshgrid(*phase_shifts_per_dim, indexing="ij")
 
         return np.ravel(sum(p for p in phase_shifts_grid))
 