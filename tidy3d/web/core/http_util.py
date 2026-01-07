7afbc6a9fc27da78a90611443efccbb90e6669d7
Marco Eppenberger <meppenberger@psiquantum.com>
2026-01-07 16:33:07 +0000

    key checking only done if result is dict, otherwise return result directly

--- tidy3d/web/core/http_util.py
+++ tidy3d/web/core/http_util.py
@@ -195,7 +195,10 @@ def http_interceptor(func: Callable[..., Any]) -> Callable[..., JSONType]:
                 log = get_logger()
                 log.warning(warning)
 
-        return result.get("data") if "data" in result else result
+            if 'data' in result:
+                return result['data']
+            
+        return result
 
     return wrapper
 
